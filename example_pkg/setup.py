f96c256a38cbe4e6a91e1656bf2546d8b5776a62
Hugh Nguyen <hughnguyen@ibm.com>
2021-02-13 08:17:38 -0800

    test

--- example_pkg/setup.py
+++ example_pkg/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="common-ev", # Replace with your own username
+    name="common-ev-hughnguyen", # Replace with your own username
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
